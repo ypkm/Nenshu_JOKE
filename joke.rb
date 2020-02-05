cf9d9a903452261f1f560bf3bd4863eb3ca9e70a
ypkm <atsushi.chem@email.com>
2020-02-05 14:54:55 +0900

    インデント修正

--- joke.rb
+++ joke.rb
@@ -1,14 +1,14 @@
 /婚活?女子☆彡アプリ/
-	puts "年収を入力してください(単位：万円)"
-	income = gets.chomp.to_i
-	if income < 200
-	 puts "帰ってください"
-	elsif income >= 200 && income < 300
-	 puts "他にアピールポイントあるの？？"
-	elsif income >= 300 && income < 400
+puts "年収を入力してください(単位：万円)"
+income = gets.chomp.to_i
+if income < 200
+	puts "帰ってください"
+elsif income >= 200 && income < 300
+	puts "他にアピールポイントあるの？？"
+elsif income >= 300 && income < 400
 	 puts "話くらいは聞いてあげます"
-	elsif income >= 400 && income < 500
+elsif income >= 400 && income < 500
 	 puts "ごはんに行きましょう。おごられてあげる。"
-	else
+else
 	 puts "あなたのこと、いいと思ってたの"
 end
