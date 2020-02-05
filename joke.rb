e1451024bb55f4fb6e75e74d2d6b733e245f284d
ypkm <atsushi.chem@email.com>
2020-02-05 15:39:46 +0900

    修正２回目

--- joke.rb
+++ joke.rb
@@ -1,4 +1,4 @@
-/婚活?女子☆彡アプリ/
+#婚活?女子☆彡アプリ
 puts "年収を入力してください(単位：万円)"
 income = gets.chomp.to_i
 if income < 200
@@ -6,9 +6,9 @@ if income < 200
 elsif income >= 200 && income < 300
 	puts "他にアピールポイントあるの？？"
 elsif income >= 300 && income < 400
-	 puts "話くらいは聞いてあげます"
+	puts "話くらいは聞いてあげます"
 elsif income >= 400 && income < 500
-	 puts "ごはんに行きましょう。おごられてあげる。"
+	puts "ごはんに行きましょう。おごられてあげる。"
 else
 	 puts "あなたのこと、いいと思ってたの"
 end
